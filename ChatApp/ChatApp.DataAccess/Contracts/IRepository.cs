def951fdc2f53c699bcb76a7e854058c4c3e2da2
mikhail_rozputnii <mikhail.rozputnii@gmail.com>
2019-07-02 16:42:34 +0300

    implement generic repository

---
+++ ChatApp/ChatApp.DataAccess/Contracts/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using ChatApp.Domains;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ChatApp.DataAccess.Contracts
+{
+    public interface IRepository<TEntity> where TEntity : class, IEntity
+    {
+        IQueryable<TEntity> GetAllByCondition(Expression<Func<TEntity, bool>> expression);
+        TEntity GetByCondition(Expression<Func<TEntity, bool>> expression);
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ ChatApp/ChatApp.DataAccess/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,54 @@
+﻿using ChatApp.DataAccess.Contracts;
+using ChatApp.Domains;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ChatApp.DataAccess.Data.Repositories
+{
+    internal class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity
+    {
+        private readonly ChatAppDbContext _dbContext;
+        public BaseRepository(ChatAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual void Create(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException();
+
+            entity.CreateDate = DateTime.UtcNow;
+            _dbContext.Set<TEntity>().Add(entity);
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException();
+            _dbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public virtual IQueryable<TEntity> GetAllByCondition(Expression<Func<TEntity, bool>> expression)
+        {
+            if (expression == null)
+                throw new ArgumentNullException();
+            return _dbContext.Set<TEntity>().Where(expression);
+        }
+
+        public virtual TEntity GetByCondition(Expression<Func<TEntity, bool>> expression)
+        {
+            if (expression == null)
+                throw new ArgumentNullException();
+            return _dbContext.Set<TEntity>().FirstOrDefault(expression);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException();
+            _dbContext.Set<TEntity>().Update(entity);
+        }
+    }
+}
