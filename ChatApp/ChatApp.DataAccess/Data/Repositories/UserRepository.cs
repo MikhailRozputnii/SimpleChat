7233a28b0dffd3e4e8ad0e0b0787835295fee301
mikhail_rozputnii <mikhail.rozputnii@gmail.com>
2019-07-02 16:43:06 +0300

    implement user repository

---
+++ ChatApp/ChatApp.DataAccess/Data/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using ChatApp.DataAccess.Contracts;
+using ChatApp.Domains;
+
+namespace ChatApp.DataAccess.Data.Repositories
+{
+    internal class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(ChatAppDbContext dbContext) : base(dbContext) { }
+    }
+}
