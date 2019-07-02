d043da4957c9e939f5b854abfbf406722f06b16f
mikhail_rozputnii <mikhail.rozputnii@gmail.com>
2019-07-02 16:46:08 +0300

    resolve DAO dependency

--- ChatApp/ChatApp/Startup.cs
+++ ChatApp/ChatApp/Startup.cs
@@ -18,6 +18,7 @@ namespace ChatApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddChatAppDbContext(Configuration);
+            services.AddRepositoryWrapper();
         }
 
 
