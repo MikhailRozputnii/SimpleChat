1265249354cbf792d654b715e2312ee65bb637c6
mikhail_rozputnii <mikhail.rozputnii@gmail.com>
2019-07-05 14:26:40 +0300

    add automapper configure

---
+++ ChatApp/ChatApp.Core/Configs/ProfileMapper/DomainProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ChatApp.Core.Dto;
+using ChatApp.Domains;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp.Core.Configs.ProfileMapper
+{
+    public class DomainProfile : Profile
+    {
+        public DomainProfile()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<UserRole, UserRoleDto>().ReverseMap();
+            CreateMap<Role, RoleDto>().ReverseMap();
+
+        }
+    }
+}
---
+++ ChatApp/ChatApp.Core/Extensions/ServicesConfig.cs
@@ -0,0 +1,21 @@
+﻿using ChatApp.Core.Dto;
+using ChatApp.Core.Services.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatApp.Core.Extensions
+{
+    public static class ServicesConfig
+    {
+        public static void AddCustomIdentityProvider(this IServiceCollection services)
+        {
+            //services.AddIdentity<UserDto, UserRoleDto>()
+            //        .AddDefaultTokenProviders();
+            services.AddIdentityCore<UserDto>()
+               .AddSignInManager()
+               .AddDefaultTokenProviders();
+            services.AddTransient<IUserStore<UserDto>, UserStore>();
+            //services.AddTransient<IRoleStore<UserRoleDto>, RoleStore>();
+        }
+    }
+}
