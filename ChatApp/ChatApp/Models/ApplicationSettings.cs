b51e209ca0ff5c716eb4718b5da86399b89f4976
mikhail_rozputnii <mikhail.rozputnii@gmail.com>
2019-07-17 18:49:41 +0300

    implement  register and login endpoints,

---
+++ ChatApp/ChatApp/Models/ApplicationSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Models
+{
+    public class ApplicationSettings
+    {
+        public const string JWT_Secret = "1q2w3e4r5t6y78u9i0o1p2a3s4d5f6";
+        public const string Client_URL = "http://localhost:53348";
+    }
+}
