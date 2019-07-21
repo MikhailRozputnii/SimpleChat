0367931513d1c1468b9874d729e325bc34c22d87
mikhail_rozputnii <mikhail.rozputnii@gmail.com>
2019-07-21 19:37:15 +0300

    add profile endpoint

---
+++ ChatApp/ChatApp/Controllers/UserProfileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatApp.Core.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private UserManager<UserDto> _userManager;
+        public UserProfileController(UserManager<UserDto> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<Object> GetUserProfile()
+        {
+            string userId = User.Claims.First(c => c.Type == "UserID").Value;
+            var user = await _userManager.FindByIdAsync(userId);
+            return new
+            {
+                user.FirstName,
+                user.LastName,
+                user.Email
+            };
+        }
+    }
+}
