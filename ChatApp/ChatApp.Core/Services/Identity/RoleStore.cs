bd7a2c875ab8deea7638d3eef063476054c332f8
mikhail_rozputnii <mikhail.rozputnii@gmail.com>
2019-07-23 17:08:46 +0300

    implement custom role store

--- ChatApp/ChatApp.Core/Services/Identity/RoleStore.cs
+++ ChatApp/ChatApp.Core/Services/Identity/RoleStore.cs
@@ -1,4 +1,7 @@
-﻿using ChatApp.Core.Dto;
+﻿using AutoMapper;
+using ChatApp.Core.Dto;
+using ChatApp.DataAccess.Contracts;
+using ChatApp.Domains;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Threading;
@@ -6,61 +9,135 @@ using System.Threading.Tasks;
 
 namespace ChatApp.Core.Services.Identity
 {
-    internal class RoleStore : IRoleStore<UserRoleDto>
+    public class RoleStore : IRoleStore<RoleDto>
     {
-        public Task<IdentityResult> CreateAsync(UserRoleDto role, CancellationToken cancellationToken)
+        private IMapper _mapper;
+        private readonly IRepositoryWrapper _repository;
+
+        public RoleStore(IMapper mapper, IRepositoryWrapper repository)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _repository = repository;
         }
 
-        public Task<IdentityResult> DeleteAsync(UserRoleDto role, CancellationToken cancellationToken)
+        public async Task<IdentityResult> CreateAsync(RoleDto role, CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+            var entity = _mapper.Map<Role>(role);
+            await Task.Run(() => _repository.RoleRepository.Create(entity));
+            _repository.Save();
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> DeleteAsync(RoleDto role, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+            var entity = _mapper.Map<Role>(role);
+            _repository.RoleRepository.Delete(entity);
+            await Task.Run(() => _repository.RoleRepository.Delete(entity));
+            return IdentityResult.Success;
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
-        public Task<UserRoleDto> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        protected virtual void Dispose(bool disposing)
         {
-            throw new NotImplementedException();
+            if (disposing)
+            {
+                _repository?.Dispose();
+            }
         }
 
-        public Task<UserRoleDto> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+        public async Task<RoleDto> FindByIdAsync(string roleId, CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            if (roleId == null)
+                throw new ArgumentNullException(nameof(roleId));
+            Guid idGuid;
+            if (!Guid.TryParse(roleId, out idGuid))
+                throw new ArgumentException("Not a valid Guid id", nameof(roleId));
+            var entity = await Task.Run(() => _repository.RoleRepository.GetByCondition(i => i.Id == idGuid));
+            var roleDto = _mapper.Map<RoleDto>(entity);
+            return roleDto;
         }
 
-        public Task<string> GetNormalizedRoleNameAsync(UserRoleDto role, CancellationToken cancellationToken)
+        public async Task<RoleDto> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            if (normalizedRoleName == null)
+                throw new ArgumentNullException(nameof(normalizedRoleName));
+            var entity = await Task.Run(() => _repository.RoleRepository.GetByCondition(i => i.Name == normalizedRoleName));
+            var roleDto = _mapper.Map<RoleDto>(entity);
+            return roleDto;
         }
 
-        public Task<string> GetRoleIdAsync(UserRoleDto role, CancellationToken cancellationToken)
+        public Task<string> GetNormalizedRoleNameAsync(RoleDto role, CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+            return Task.FromResult(role.Name);
         }
 
-        public Task<string> GetRoleNameAsync(UserRoleDto role, CancellationToken cancellationToken)
+        public Task<string> GetRoleIdAsync(RoleDto role, CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+            return Task.FromResult(role.Id.ToString());
         }
 
-        public Task SetNormalizedRoleNameAsync(UserRoleDto role, string normalizedName, CancellationToken cancellationToken)
+        public Task<string> GetRoleNameAsync(RoleDto role, CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+            return Task.FromResult(role.Name);
         }
 
-        public Task SetRoleNameAsync(UserRoleDto role, string roleName, CancellationToken cancellationToken)
+        public Task SetNormalizedRoleNameAsync(RoleDto role, string normalizedName, CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+            else if (normalizedName == null)
+                throw new ArgumentNullException(nameof(normalizedName));
+            role.Name = normalizedName;
+            return Task.FromResult<object>(null);
         }
 
-        public Task<IdentityResult> UpdateAsync(UserRoleDto role, CancellationToken cancellationToken)
+        public Task SetRoleNameAsync(RoleDto role, string roleName, CancellationToken cancellationToken = default(CancellationToken))
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+            else if (roleName == null)
+                role.Name = roleName;
+            return Task.CompletedTask;
+        }
+
+        public async Task<IdentityResult> UpdateAsync(RoleDto role, CancellationToken cancellationToken=default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+
+            var entity = await Task.Run(() => _repository.RoleRepository.GetByCondition(i => i.Id == role.Id));
+            if (entity == null)
+                return IdentityResult.Failed(new IdentityError { Description = $"Could not update role {role.Name}." });
+
+            entity.Name = role.Name;
+            await Task.Run(() => _repository.RoleRepository.Update(entity));
+            _repository.Save();
+            return IdentityResult.Success;
         }
     }
 }
